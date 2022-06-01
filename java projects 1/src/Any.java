973b9552d44dfa1808284368559cc5e627ddbd62
Furkan <simsekfurkann07@gmail.com>
2022-06-02 01:33:43 +0300

    a

--- java projects 1/src/Any.java
+++ java projects 1/src/Any.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class Any {
     public static void main(String[] args) {
-        String name = "Furkan";
+        String name = "Furkan Any";
 
         System.out.println(name);
     }
